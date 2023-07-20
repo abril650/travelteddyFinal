8504414c0ca42ad4bda78f33fe99983367f7b919
abril650 <purxpleabrill@gmail.com>
2023-07-20 15:40:32 -0700

    finaal1

--- travelteddy/IndiaNormView.swift
+++ travelteddy/IndiaNormView.swift
@@ -9,9 +9,59 @@ import SwiftUI
 
 struct IndiaNormView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        ZStack{
+                Color(UIColor(red: 245/255.0, green: 222/255, blue: 179/255.0, alpha: 1))
+                  .ignoresSafeArea()
+                VStack{
+                  Text("Hindi Translation!!!")
+                    .font(.subheadline)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                  HStack
+                  {
+                    Text("Hindi:")
+                      .frame(width: 150, height: 90, alignment: .center)
+                      .border(Color.black, width: 5)
+                    Text("English:")
+                      .frame(width: 150, height: 90, alignment: .center)
+                      .border(Color.black, width: 5)
+                  }//h
+                  HStack
+                  {
+                    Text("Namaste")
+                      .frame(width: 150, height: 150, alignment: .center)
+                      .border(Color.black, width: 5)
+                    Text("Hi!")
+                      .frame(width: 150, height: 150, alignment: .center)
+                      .border(Color.black, width: 5)
+                  }//h
+                  HStack
+                  {
+                    Text("Alvida!")
+                      .frame(width: 150, height: 150, alignment: .center)
+                      .border(Color.black, width: 5)
+                    Text("GoodBye")
+                      .frame(width: 150, height: 150, alignment: .center)
+                      .border(Color.black, width: 5)
+                  }//h
+                  HStack
+                  {
+                    Text("Dhanyavaad")
+                      .multilineTextAlignment(.center)
+                      .frame(width: 150, height: 150)
+                      .border(Color.black, width: 5)
+                    Text("Thank You")
+                      .padding(.horizontal, 5.0)
+                      .frame(width: 150, height: 150, alignment: .center)
+                      .border(Color.black, width: 5)
+                  }//h
+                }
+              }
+              //  Image()
+            }
+          }//zstack
+    
+
 
 struct IndiaNormView_Previews: PreviewProvider {
     static var previews: some View {
--- travelteddy/travelteddyApp.swift
+++ travelteddy/travelteddyApp.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-@main
+
 struct travelteddyApp: App {
     var body: some Scene {
         WindowGroup {
